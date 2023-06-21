f45a79cd7e8bd11975a55ca752c97ff73654c39e
itsCyriac <133679986+itsCyriac@users.noreply.github.com>
2023-06-21 18:39:21 +0530

    Add files via upload
    v3

--- Dockerfile
+++ Dockerfile
@@ -3,41 +3,8 @@ FROM ubuntu:latest
 # Install dependencies
 RUN apt-get update && apt-get install -y curl gnupg2
 
-# Install Trivy
-RUN curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
-
-# Install Dive
-RUN curl -L https://github.com/wagoodman/dive/releases/download/v0.10.0/dive_0.10.0_linux_amd64.tar.gz | tar -xvz \
-    && mv dive /usr/local/bin/
-
-# Install Grype
-RUN curl -L https://github.com/anchore/grype/releases/latest/download/grype-linux-amd64 -o /usr/local/bin/grype \
-    && chmod +x /usr/local/bin/grype
-
-# Install Syft
-RUN curl -L https://github.com/anchore/syft/releases/latest/download/syft-linux-amd64 -o /usr/local/bin/syft \
-    && chmod +x /usr/local/bin/syft
-
-# Install Dagda
-#RUN curl -L https://github.com/eliasgranderubio/dagda/archive/refs/tags/v0.8.3.tar.gz | tar -xvz \
-#    && mv dagda-0.8.3 /opt/dagda \
-#    && ln -s /opt/dagda/dagda.py /usr/local/bin/dagda
-
 # Install Anchore
 RUN curl https://anchore.io/getting-started/install.sh | bash
-
-# Install Clair
-#RUN echo 'deb http://httpredir.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list \
-#    && curl https://raw.githubusercontent.com/jessfraz/dockerfiles/master/jessie/usr/local/bin/add-apt-repository -o /usr/local/bin/add-apt-repository \
-#    && chmod +x /usr/local/bin/add-apt-repository \
-#    && add-apt-repository "deb http://httpredir.debian.org/debian stretch-backports main" \
-#    && apt-get update \
-#    && apt-get install -y clair
-
-# Install Calico
-RUN curl -O -L https://github.com/projectcalico/calicoctl/releases/download/v3.19.1/calicoctl \
-    && chmod +x calicoctl \
-    && mv calicoctl /usr/local/bin/
 
 RUN apt install figlet
 
--- scan-image.sh
+++ scan-image.sh
@@ -1,50 +1,67 @@
 #!/bin/bash
 
+curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
+curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+echo ""
+echo ""
 figlet Container Security
+echo ""
+echo ""
+echo "OPTIONS"
+echo "1.Scan a docker image for Vulnerability (from Docker Hub)"
+echo "2.Scan a docker image for Inventory (SBOM)"
+echo "3.List out packages for a docker image"
+echo "4.Scan a remote repository for vulnerabilty "
+echo "5.Exit"
 
-echo "Enter path to the Docker image tar file:"
-read -e -p "Image Path: " image_path
+echo "Eneter an option (Number)"
+read option
 
-echo "Select a vulnerability scanning tool:"
-echo "1. Trivy"
-echo "2. Dive"
-echo "3. Grype"
-echo "4. Syft"
-echo "5. Dagda"
-echo "6. Anchore"
-echo "7. Clair"
-echo "8. Calico"
-read -p "Enter option number: " option
+if [ $option -ne 5 ]
+then
+    if [ $option -eq 1 ]
+    then
+        echo ""
+        echo ""
+        echo "Enter a container name"
+        echo ""
+        read con_name
+        echo ""
+        grype $con_name
+        echo ""
+    elif [ $option -eq 2 ]
+    then
+        echo ""
+        echo ""
+        echo "Enter a container name"
+        echo ""
+        read con_name
+        echo ""
+        syft $con_name
+        echo ""
+    elif [ $option -eq 3 ]
+    then
+        echo ""
+        echo ""
+        echo "Enter a container name for listing packages"
+        echo ""
+        read con_name
+        echo ""
+        syft packages $con_name
+        echo ""
+    elif [ $option - eq 4 ]
+    then
+        echo ""
+        echo ""
+        echo "Enter link to repository"
+        echo ""
+        read link
+        trivy repo $link
+    else
+        echo "Wrong Input"
+    fi
+else
+    echo "Exiting"
+fi
 
-case $option in
-    1)
-        trivy image -f $image_path
-        ;;
-    2)
-        dive $image_path
-        ;;
-    3)
-        grype $image_path
-        ;;
-    4)
-        syft $image_path
-        ;;
-    5)
-        dagda image -i $image_path
-        ;;
-    6)
-        anchore-cli image add $image_path
-        anchore-cli image wait $image_path
-        anchore-cli image content $image_path
-        ;;
-    7)
-        clair-scanner -c http://localhost:6060 --ip $(hostname -i) -r $image_path
-        ;;
-    8)
-        calicoctl image load -f $image_path
-        ;;
-    *)
-        echo "Invalid option"
-        exit 1
-        ;;
-esac
